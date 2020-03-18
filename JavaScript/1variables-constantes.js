211fcaad542c9ad35ce12e31ebdbf27a9cbd6862
Luis Salinas <Luis-Salinas-SD>
2020-03-18 11:57:46 -0600

    variables, operaciones matematics

---
+++ JavaScript/1variables-constantes.js
@@ -0,0 +1,29 @@
+    /*Las variables son espacios de memoria reservados para un valor, este puede ser reasignalble.
+    sintaxis:
+    se puede declarar con "var" ó "let"
+    */
+    let nombre = "Luis";
+    var apellido = prompt('¿Cual es su apellido?');
+    /*Las constantes son espacios de memoria pero el valor no puede ser reasignado.
+    sintaxis:
+    se pueden declarar con "let"
+    */
+   const pi = 3.1416;
+    
+
+    var manzana = 10, peras = 5;
+    var frutas = manzana + peras;
+
+    var op = (10 + 2) / 2 + 4 * 2;
+
+     var igual;
+     igual= ('a' === 'a'); //true
+
+     var desigual;
+     desigual = ('adios' !== 'hola'); //true
+
+     var mayor;
+     mayor = (6 >= 6); //true
+
+     var menor;
+     menor = (6 <=6 ); //true
---
+++ JavaScript/2numeros-y-operadores-matematicos.js
@@ -0,0 +1,25 @@
+//Operaciones aritmeticas basicas.
+let suma = 2+2;
+let resta = 2-2;
+let division = 10/2;
+let mult = 2*2;
+
+/* Libreria Math para ejecuta operaciones más complejas.*/
+
+//rretornar el valor de pi:
+Math.PI
+
+// 3.141592653589793
+
+//potencias elevra 5 a la 3ra potencia
+
+Math.pow(5,3);
+//125
+//redondear decimales
+
+Math.round(5.6);
+//7
+
+//raiz
+Math.sqrt(9);
+//3
