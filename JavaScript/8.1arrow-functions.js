b1171cf906d2bcfc6d217caf634ee2ef87e6a33f
Luis-Salinas-SD <paus.impala21@gmail.com>
2020-04-30 18:46:12 -0500

    arrow-function

---
+++ JavaScript/8.1arrow-functions.js
@@ -0,0 +1,12 @@
+//# Funcion convensional
+/* function suma(a,b) {
+    return a+b;
+
+}console.log(suma(5,5)); */
+//# Funcion con [Array-Functions]
+/* suma = (a,b)=> a + b
+console.log(suma(5,6)); */
+
+
+
+
