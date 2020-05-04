3169e667c2362bf0fd3a83d0c830dd85f8a5b873
Luis-Salinas-SD <paus.impala21@gmail.com>
2020-05-04 13:41:17 -0500

    Actu-ciclos

--- JavaScript/7ciclos.js
+++ JavaScript/7ciclos.js
@@ -1,21 +1,23 @@
 //# +++++++++++++++++++++++++++++++++++++++++++++++++++++++ CICLOS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-/*
-var numero = 1;
-while(numero <10){
-    alert(numero);
-    numero = numero + 1;
-
-}*/
-
-
-for (var i = 1; i <= 15; i++) {
+//! Ciclo for
+/* for (var i = 1; i <= 15; i++) {
       alert(i);
-}
+} */
 
-/*
-let num =0;
+//$ Ciclo do-while
+
+/* let num =0;
 do {
    console.log(num);
   num ++
-} while (num <= 5);
-*/
+} while (num <= 5); */
+
+//$ciclo while
+
+var numero = 1;
+while(numero <10){
+//   alert(numero);
+   numero = numero + 1;
+console.log(numero);
+
+}
--- JavaScript/8funciones.js
+++ JavaScript/8funciones.js
@@ -1,27 +1,29 @@
 //! En una funcion imprime un numero del 0 hasta el que el usuario quiere.
-/*
-var num = prompt("introduce un numero");
+
+/* var num = prompt("introduce un numero");
 
 function cuenta(num){
     for(var i=1; i<=num; i++){
         document.write(i+"\n");
     }
- }cuenta(num);
-*/
+ }cuenta(num); */
+
 //? Funcion que convierte cadenas con minusculas a cadenas con mayusculas
 
-/* var nom = prompt("Ingresa tu nombre");
+/*
+var nom = /* prompt("Ingresa tu nombre"); "luis"
 
-function NombreMayus(){
+function NombreMayus(nom){
 nombre = nom.toUpperCase()
-document.write(nombre)
-}NombreMayus() */
+console.log(nombre);
+
+}NombreMayus(nom); */
 
 //! Funcion que calcula el cuadrado de una funcion
 /* function cuadrado(numero) {
     return numero * numero;
 }
-console.log( cuadrado(5)) */
+console.log( cuadrado(5)); */
 
 
 //? Funcion dentro de otra Funcion
@@ -38,10 +40,11 @@ accion(decirHola); */
 
 //?o
 
-/* function accion(hablar) {
-    hablar();
-    }
+function accion(hablar) {
+hablar()
+}
 
-    accion(function decirHola(){
-        console.log("hola amicos como estan");
-    }); */
+accion(function decirHola(){
+    console.log("hola amicos como estan");
+    }
+);
