4e0edc6b7c0b9a3db92402573fc8fa60dfd7233b
Luis Salinas <Luis-Salinas-SD>
2020-05-31 19:24:25 -0500

    Actualización

--- JavaScript/11arreglos.js
+++ JavaScript/11arreglos.js
@@ -19,8 +19,16 @@ let array = [50, 53, 100, 45];
 
  // array.unshift("hola"); //agrega un elemento al incio
  // array.shift(); //quita un elemento al inicio
-console.log(array);
+//console.log(array);
 
 //# agregar y quitar elementos de un arreglo en una posicion personalizada:
-let name = ["luis", "ana", "dan", "sergio"];
+let names = ["luis", "ana", "dan", "sergio"];
+//             0       1     2        3
+//!splice(#indice en el que se agrgaran, #cantidad de elemento que se van a eliminar #valores que se van a aregar 1,2,3...)
 
+//!Agrega los valores juan y lola en la posicion 3
+names.splice(3,0, 5, 8);
+
+//!Elimina el valor o valores en la poscicon descrita en este caso eliminaos 3 elementos en la poscion 3
+//names.splice(0,3);
+console.log(names);
