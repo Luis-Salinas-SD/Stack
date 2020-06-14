9e99c84b8336087715ba76b13804bbce150f9c9b
Luis-Salinas-SD <paus.impala21@gmail.com>
2020-06-13 22:35:00 -0500

    Actualizacion

--- JavaScript/22objeto-json.js
+++ JavaScript/22objeto-json.js
@@ -4,22 +4,22 @@ let video = {
     titulo: "piloto",
     duración: 2,
     formato: "avi",
-    //?arreglo
+    //!arreglo
     arreglo: ["uno", "dos", "tres"],
     //!metodo
     inscribir: function (usuario) {
         console.log(usuario + " Esta inscrito");
-
     }
 }
-//* invocar una propiedad el objeto.
+//! invocar una propiedad el objeto.
 /* console.log(video.arreglo); */
-//* Invocar metodo
+//! Invocar metodo
  /* video.inscribir("Juan"); */
-//*Editar alguna propiedad de nuestro objeto
+//* Editar alguna propiedad de nuestro objeto
 /* video.titulo = "Prueba";
 console.log(video.titulo); */
 //* Editar alguna metodo de nuestro objeto
 /* video.inscribir = function () {
 console.log("hola soy la funcion editada");
 }*/
+
--- JavaScript/23funciones-constructoras.js
+++ JavaScript/23funciones-constructoras.js
@@ -1,23 +1,22 @@
-//# Funciones constructoras
+//# ############################################## Funciones constructoras ##############################################
 
 //*Funcion constructora
-function Curso(titulo) {
+function Curso(titulo, duracion, formato) {
     //! Propiedades
     this.titulo = titulo;
-    this.duracion = 2;
-    this.formato = "avi;";
+    this.duracion = duracion;
+    this.formato = formato;
 
     //! Metodos
     this.inscribir = function (usuario) {
         console.log(usuario + "Está Inscrito");
-
     }
 }
 //*Creación de un objeto
-let cursoJavaScript = new Curso("curso java pro");
+let cursoJavaScript = new Curso("curso java pro", "5hrs", "Mp4");
 let cursoRuby = new Curso("Curso de Ruby Pro")
 
-console.log(cursoJavaScript.titulo );
+console.log(cursoJavaScript.titulo, cursoJavaScript.duracion, cursoJavaScript.formato);
 console.log(cursoRuby.titulo);
 
 
