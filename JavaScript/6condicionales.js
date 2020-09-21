ba0ccf25149e781bd655baee4a76150bccf45779
Luis-Salinas-SD <paus.impala21@gmail.com>
2020-09-21 00:01:56 -0500

    a

--- JavaScript/6condicionales.js
+++ JavaScript/6condicionales.js
@@ -1,19 +1,19 @@
 //# condicional IF
-if(24 === 24 || "hola" === "hola"){
-    alert(true);
-  }else{
-    alert(false);
-  }
-  //true
+if (24 === 24 || "hola" === "hola") {
+  alert(true);
+} else {
+  alert(false);
+}
+//true
 
 //! Condicional if()else
- let nota = 10;
- if (nota = 7) {
-     console.log("malo");
- }else if (nota = 8) {
-     console.log("regular");
- }else if (nota = 9) {
-     console.log("bueno");
-}else{
-    console.log("Excelente");
+let nota = 10;
+if (nota = 7) {
+  console.log("malo");
+} else if (nota = 8) {
+  console.log("regular");
+} else if (nota = 9) {
+  console.log("bueno");
+} else {
+  console.log("Excelente");
 }
